ae06c242eba55f4728787443e2cc8552efc14327
Marven <marvenwilsons@gmail.com>
2020-12-14 21:27:02 -0500

    date coverage prog 2

--- components/asset-credit/settings/coverage.vue
+++ components/asset-credit/settings/coverage.vue
@@ -2,12 +2,46 @@
     <div class="relative flex fullheight-percent fullwidth" style="overflow-y:auto" >
         <div class="fullwidth absolute flexcol flex fullheight-percent pad125" >
             <small>
-                Set Date Coverage
+                <strong>Set Date Coverage</strong>
             </small>
-            <hr>
+            <v-divider class="margintop050 marginbottom050 fullwidth" ></v-divider>
+            <div v-if="error" class="backgrounderr padleft050 flex spacebetween flex flexcenter " >
+                <small>
+                    {{error}}
+                </small>
+                <button @click="error = undefined" class="padleft050 padright050" >
+                    <small>Ok</small>
+                </button>
+            </div>
             <!-- from -->
-            <div style="background: whitesmoke" class="">
+            <div class="margintop050">
                 <small><strong>From:</strong></small> <br>
+                <small>
+                    Year: 
+                    <select class="borderRad4 padleft050" style=" width:60px;" name="" id="">
+                        <option class="flex flexcenter" v-for="year in availableYearCoverage" :key="year" value="">
+                            <div class="pad050" >{{year}}</div>
+                        </option>
+                    </select>
+                </small>
+                <small>
+                    Month:
+                    <select style="border:1px solid gray; width:60px;" name="" id="">
+                        <option v-for="month in months" :key="month" value="">
+                            {{month}}
+                        </option>
+                    </select>
+                </small>
+                <small>
+                    Day:
+                    <select style="border:1px solid gray; width:60px;" name="" id="">
+                        <option value="">Day</option>
+                    </select>
+                </small>
+            </div>
+            <!-- to -->
+            <div class="marginbottom125 padbottom125 margintop050" >
+                <small><strong>To:</strong></small> <br>
                 <small>
                     Year: 
                     <select style="border:1px solid gray; width:60px;" name="" id="">
@@ -19,35 +53,15 @@
                 <small>
                     Month:
                     <select style="border:1px solid gray; width:60px;" name="" id="">
-                        <option value="">2020</option>
+                        <option v-for="month in months" :key="month" value="">
+                            {{month}}
+                        </option>
                     </select>
                 </small>
                 <small>
                     Day:
                     <select style="border:1px solid gray; width:60px;" name="" id="">
-                        <option value="">2020</option>
-                    </select>
-                </small>
-            </div>
-            <!-- to -->
-            <div>
-                <small><strong>To:</strong></small> <br>
-                <small>
-                    Year: 
-                    <select style="border:1px solid gray; width:60px;" name="" id="">
-                        <option value="">2020</option>
-                    </select>
-                </small>
-                <small>
-                    Month:
-                    <select style="border:1px solid gray; width:60px;" name="" id="">
-                        <option value="">2020</option>
-                    </select>
-                </small>
-                <small>
-                    Day:
-                    <select style="border:1px solid gray; width:60px;" name="" id="">
-                        <option value="">2020</option>
+                        <option value="">Day</option>
                     </select>
                 </small>
             </div>
@@ -58,7 +72,8 @@
 <script>
 export default {
     data: () => ({
-        availableYearCoverage: ['2020','2019'],
+        error: undefined,
+        availableYearCoverage: ['2020','2019','2018'],
         availableMonthCoverage: [],
         availableDayCoverage: [],
         months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -70,3 +85,11 @@ export default {
     }
 }
 </script>
+
+<style>
+select {
+    border:1px solid #afafaf !important;
+    border-radius: 4px !important;
+    padding-left: 8px;
+}
+</style>
