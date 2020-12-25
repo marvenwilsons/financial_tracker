7ffaea07750ef37581d178d2656be8a80e624f64
Marven <marvenwilsons@gmail.com>
2020-12-25 13:47:54 -0500

    refactored CREATE TABLE query

--- dat.sql
+++ dat.sql
@@ -1,23 +1,21 @@
-CREATE TABLE statement (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
+CREATE TABLE users (
+    user_id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    email VARCHAR(500) NOT NULL,
+    user_password VARCHAR(250) NOT NULL,
+    user_settings jsonb NOT NULL,
+    user_notes jsonb NOT NULL
+);
+
+CREATE TABLE _statement (
+    id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     date date NOT NULL,
     description TEXT NOT NULL,
     statement_type VARCHAR(250) NOT NULL,
     widthdrawn_amount MONEY NOT NULL,
     deposited_amount MONEY NOT NULL,
     balance_amount MONEY NOT NULL,
-    transaction_purpose VARCHAR(250) NOT NULL
+    transaction_purpose VARCHAR(250) NOT NULL,
+    statement_owner TEXT NOT NULL REFERENCES users(user_id)
 );
-
-INSERT INTO statement (date,         description,       widthdrawn_amount, deposited_amount, statement_type, transaction_purpose, balance_amount) VALUES                                 19:23:24
-(to_date('10-08-2020','MM-DD-YYYY'), 'PRESTO',              20.00, 0, 1,    essential,         4099.74),
-(to_date('10-08-2020','MM-DD-YYYY'), 'DOLLARAMA  ',         12.15, 0, 1,    food-essential,    4111.89),
-(to_date('10-08-2020','MM-DD-YYYY'), 'NICK  MIRAS NO FRILL',11.66, 0, 1,    Investment,        4123.55)
-
-
-CREATE TABLE statement_type (
-    id BIGSERIAL NOT NULL PRIMARY KEY,
-    type_name VARCHAR(255) NOT NULL
-);
-
-INSERT INTO statement_type (type_name) VALUES ('debit statement');
